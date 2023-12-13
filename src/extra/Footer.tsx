563850c3ab6928bbfb70647240a9b67cc8ad79e1
Elliot <elliotedwinmoses2020@gmail.com>
2023-12-13 16:57:53 +0200

    started

---
+++ src/extra/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="https://www.frontendmentor.io/profile/MosesElite69">MosesElite69</a>.
+        </footer>
+    )
+}
+
+export default Footer
