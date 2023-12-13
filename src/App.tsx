db2743aca7f4cd50f5f6542c22cfb526d9b9fc5e
Elliot <elliotedwinmoses2020@gmail.com>
2023-12-13 16:59:24 +0200

    started

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import luxury from "./assets/icon-luxury.svg"
 import sedans from "./assets/icon-sedans.svg"
 import suvs from "./assets/icon-suvs.svg"
+import MainStuff from "./extra/MainStuff"
+import Footer from "./extra/Footer"
 
 function App() {
 
   return (
     <>
+      <MainStuff />
       <img src={luxury} />
       <img src={sedans} />
       <img src={suvs} />
       <h1 className='underline text-3xl text-bold'>Hello World</h1>
+      <Footer />
     </>
   )
 }
